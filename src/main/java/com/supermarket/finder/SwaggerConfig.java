37aa375c007f644bc76b3ea61d050cb17e57ccec
irabanal <irabanal@izertis.com>
2023-10-15 16:38:06 +0200

    Project definition

--- src/main/java/com/supermarket/finder/SwaggerConfig.java
+++ src/main/java/com/supermarket/finder/SwaggerConfig.java
@@ -2,18 +2,22 @@ package com.supermarket.finder;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+    info = @Info(title = "supermasbarato.es - API", version = "v1", description = "API para la búsqueda de productos en supermercados"))
 public class SwaggerConfig {
 
   @Bean
   public OpenAPI customOpenAPI() {
     final Server server = new Server();
-    server.setUrl("https://supermasbarato.es/superfinder");
+    server.setUrl("https://supermasbarato.es/superfinder/");
     return new OpenAPI().servers(List.of(server));
   }
 }
